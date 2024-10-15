df5332d4ec6fcd7e21b0ed6b1972da75df6eadc0
Rodrigo Barreto <rodrigobarreto744@gmail.com>
2024-10-15 19:43:57 -0300

    add firebase no env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCvPfQEF-fTOV-Qsa0MDMd6g2xNZZ2t2-I",
+    authDomain: "cadclienteapp-ac12a.firebaseapp.com",
+    projectId: "cadclienteapp-ac12a",
+    storageBucket: "cadclienteapp-ac12a.appspot.com",
+    messagingSenderId: "137283993299",
+    appId: "1:137283993299:web:3f65472e8f3f20b0515e26",
+    measurementId: "G-4TCTRH140P"
+  }
 };
